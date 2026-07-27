fe1e157a0beb6b2267f85df6a21cf7b54508a32b
Abymanyu Nwr <abymanyunwr03@gmail.com>
2026-07-28 00:35:57 +0700

    fix: import DirectoryBruteforcer as DirBruteforcer

--- tools/scanner/__init__.py
+++ tools/scanner/__init__.py
@@ -1,6 +1,6 @@
 from .vuln_scanner import VulnScanner
 from .sql_injection import SQLInjectionScanner
 from .xss_scanner import XSSScanner
-from .dir_bruteforce import DirBruteforcer
+from .dir_bruteforce import DirectoryBruteforcer as DirBruteforcer
 from .ssl_checker import SSLChecker
 from .cors_scanner import CORSScanner
